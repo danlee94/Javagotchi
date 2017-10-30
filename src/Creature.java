f54a6d466eaacc5b3c4b4879a184642d1c71da3c
Daniel Lee <dan.lee@live.co.uk>
2017-10-30 11:31:11 +0000

    Created Creature java file, and empty Main class

---
+++ src/Creature.java
@@ -0,0 +1,80 @@
+/**
+ * Created by k1764092 on 30/10/17.
+ */
+public class Creature {
+    int energy, fullness, happiness;
+    String name;
+
+    public Creature(String name) {
+        energy = 15;
+        fullness = 15;
+        happiness = 15;
+
+        this.name = name;
+    }
+
+    public void eat(){
+        if (fullness >= 12){
+            System.out.println("Woops, " + this.getName + " is already full.");
+        } else {
+            fullness += 8;
+            happiness -= 6;
+            energy -= 4;
+
+            if (fullness > 15) {fullness = 15; }
+
+            System.out.println("That was a very delicious!");
+        }
+    }
+
+    public void sleep(){
+        if (sleep >= 12){
+            System.out.println("Woops, " + this.getName + " is not tired.");
+        } else {
+            fullness -= 5;
+            happiness -= 5;
+            energy += 8;
+
+            System.out.println (" That was a good nap!");
+            if (energy > 15) {
+                energy = 15;
+            }
+        }
+    }
+
+    public void play() {
+
+            fullness -= 4;
+            happiness += 8;
+            energy -= 6;
+
+            if (happiness > 15) {
+                happiness = 15;
+            }
+
+        System.out.println ("That was fun, let's do it again!");
+    }
+
+    public String checkStatus(){
+        String status = "";
+
+        if (happiness <= 6) {
+            status += "Oh, " + this.getName() +" is feeling sad!\n";
+        }
+
+        if (energy <=6) {
+            status += "Hmm, " + this.getName() + " is really tired!\n";
+        }
+
+        if (fullness <= 6) {
+            status += "Woops, " + this.getName() + " is super hungry!\n";
+        }
+    }
+
+    public String getName(){
+        return name;
+    }
+
+
+
+}
